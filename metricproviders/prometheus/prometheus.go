95c442a1b8a91256f0aba5737ccb2c7a6da29241
Danny Thomson <dthomson25@gmail.com>
2019-12-01 07:33:19 -0600

    Update dependencies for istio/client-go

--- metricproviders/prometheus/prometheus.go
+++ metricproviders/prometheus/prometheus.go
@@ -50,7 +50,7 @@ func (p *Provider) Run(run *v1alpha1.AnalysisRun, metric v1alpha1.Metric) v1alph
 		return metricutil.MarkMeasurementError(newMeasurement, err)
 	}
 
-	response, err := p.api.Query(ctx, query, time.Now())
+	response, _, err := p.api.Query(ctx, query, time.Now())
 	if err != nil {
 		return metricutil.MarkMeasurementError(newMeasurement, err)
 	}
